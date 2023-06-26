8ec63a94efb851217bb03b9d18b477ccbbad14c4
Zackari-Johnson <109993116+Zackari-Johnson@users.noreply.github.com>
2023-06-26 15:38:37 -0700

    Replaced tilemap vine and pad sprites
    added animations to pad

---
+++ Excavation/Assets/Scripts/ShroomPad.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShroomPad : MonoBehaviour
+{
+
+    private SpriteRenderer sr;
+
+    [SerializeField] private Sprite[] frames;
+    [SerializeField] float[] keyTimes;
+    [SerializeField] float waitTime;
+    private float waitTimer;
+
+    public float bounceForce;
+
+    private bool isExtended;
+    private bool isRetracting;
+
+    void Start()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (waitTimer > 0 && isExtended)
+        {
+            waitTimer -= Time.deltaTime;
+        }
+        else if (waitTimer <= 0 && isExtended)
+        {
+            waitTimer = 0;
+            StartCoroutine(RetractAnim());
+        }
+    }
+    void OnTriggerEnter2D(Collider2D body)
+    {
+        if (!isExtended)
+        {
+            StartCoroutine(ExtendAnim());
+        }
+        
+        BouncePlayer(body);
+
+        waitTimer = waitTime;
+    }
+
+    IEnumerator ExtendAnim()
+    {
+        sr.sprite = frames[0];
+        yield return new WaitForSeconds(keyTimes[0]);
+        sr.sprite = frames[1];
+        yield return new WaitForSeconds(keyTimes[1]);
+        sr.sprite = frames[2];
+
+        isExtended = true;
+
+        
+
+    }
+
+    IEnumerator RetractAnim()
+    {
+        if (!isRetracting)
+        {
+            isRetracting = true;
+            sr.sprite = frames[3];
+            yield return new WaitForSeconds(keyTimes[2]);
+            sr.sprite = frames[4];
+            yield return new WaitForSeconds(keyTimes[3]);
+            sr.sprite = frames[0];
+
+            isExtended = false;
+            isRetracting = false;
+        }
+        
+    }
+
+    void BouncePlayer(Collider2D body)
+    {
+        Rigidbody2D rb = body.GetComponent<Rigidbody2D>();
+
+        if (!isExtended)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, bounceForce);
+        }
+        else
+        {
+            rb.velocity = new Vector2(rb.velocity.x, bounceForce / 2);
+        }
+       
+    }
+}
---
+++ Excavation/Assets/Scripts/SpikesContainer.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikesContainer : MonoBehaviour
+{
+    [SerializeField] private GameObject spikePrefab;
+    public int numSpikes;
+
+    void Start()
+    {
+        GenerateSpikes();
+    }
+
+    void GenerateSpikes()
+    {
+        for (int i = 0; i < numSpikes; i++)
+        {
+
+            GameObject nextSpike = Instantiate(spikePrefab);
+            nextSpike.transform.parent = transform;
+            nextSpike.transform.position = transform.position + new Vector3(i * 0.5f, 0, 0);
+            GameObject prevSpike = nextSpike;
+
+        }
+    }
+}
